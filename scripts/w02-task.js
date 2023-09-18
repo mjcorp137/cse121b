8d556a71a5a017948443df964e70f35fd8ae9031
ZimaBlue1337 <144638087+mjcorp137@users.noreply.github.com>
2023-09-18 19:17:29 +0800

    Update w02-task.js

--- scripts/w02-task.js
+++ scripts/w02-task.js
@@ -8,15 +8,17 @@ let currentYear = '2023'
 let profilePicture = 'images/myPicture.JPG'
 
 /* Step 3 - Element Variables */
-const nameElement = document.getElementById();
-
+const nameElement = document.getElementById('name');
+const foodElement = document.getElementById('year');
+const yearElement = document.querySelector('#year');
 
 
 
 /* Step 4 - Adding Content */
-
-
-
+const imagesElement = document.createElement("home");
+imagesElement.setAttribute("src", "images/myPicture.JPG");
+imagesElement.setAttribute("alt", "My Profile Picture");
+document.body.appendChild(imagesElement);
 
 
 
