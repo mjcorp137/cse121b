7ae580d1fbc6f7ee3cbe5efa16ef8e2f0499179b
ZimaBlue1337 <144638087+mjcorp137@users.noreply.github.com>
2023-09-26 18:07:33 +0800

    new

---
+++ scripts/w03-task.js
@@ -0,0 +1,34 @@
+/* LESSON 3 - Programming Tasks */
+
+/* FUNCTIONS */
+function add (number1, number2){
+
+    return number1 + number2;
+}
+/* Function Definition - Add Numbers */
+
+
+/* Function Expression - Subtract Numbers */
+
+
+/* Arrow Function - Multiply Numbers */
+
+
+/* Open Function Use - Divide Numbers */
+
+
+/* Decision Structure */
+
+
+/* ARRAY METHODS - Functional Programming */
+/* Output Source Array */
+
+/* Output Odds Only Array */
+
+/* Output Evens Only Array */
+
+/* Output Sum of Org. Array */
+
+/* Output Multiplied by 2 Array */
+
+/* Output Sum of Multiplied by 2 Array */
