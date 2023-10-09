86e082421a80054725c83bef8c4186433e50dfa4
mjcorp137 <savediamahonri@gmail.com>
2023-10-10 05:49:00 +0800

    nice

---
+++ scripts/w05-task.js
@@ -0,0 +1,23 @@
+/* W05: Programming Tasks */
+
+/* Declare and initialize global variables */
+const templesElement = getElementId("temples");
+
+/* async displayTemples Function */
+
+
+
+
+/* async getTemples Function using fetch()*/
+
+
+/* reset Function */
+
+
+/* sortBy Function */
+
+
+
+getTemples();
+
+/* Event Listener */
