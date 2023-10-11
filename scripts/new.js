0093bf393def8c9433b613ca42356807b85fb28e
mjcorp137 <savediamahonri@gmail.com>
2023-10-11 09:34:32 +0800

    new

---
+++ scripts/new.js
@@ -0,0 +1,65 @@
+/* W05: Programming Tasks */
+
+/* Declare and initialize global variables */
+const templesElement = document.getElementsByTagName("div");
+let templeList = [];
+
+/* async displayTemples Function */
+const displayTemples = (temples) => 
+{
+    temples.forEach(temple => {
+        const article = document.createElement("article");
+        const h3 = document.createElement("h3");
+        h3.innerText = temple.templeName;
+        const image = document.createElement("img");
+        image.setAttribute("src", temple.imageUrl);
+        image.setAttribute("alt", temple.location);
+        article.appendChild(image);
+        article.appendChild(h3);
+        templesElement.appendChild(article);
+     
+        
+        
+    });
+}
+
+
+
+/* async getTemples Function using fetch()*/
+const getTemples = async () => {
+    const response = await fetch("https://byui-cse.github.io/cse121b-ww-course/resources/temples.json");
+    templeList = response.json();
+
+    displayTemples(templeList);
+
+}
+
+
+/* reset Function */
+function reset() {
+    templesElement.article.remove();
+}
+
+/* sortBy Function */
+function sortby (temples) {
+    reset();
+    const filter = document.getElementById("sortBy").value;
+    switch (filter){
+        case "utah":
+            displayTemples(temples.filter(temple => temple.location.match("Utah") == "Utah"));
+            break;
+        case "nonutah":
+            displayTemples(temples.filter(temple => temple.location != temple.location.match("Utah")));
+            break;
+        case "older":
+            displayTemples(temples.filter(temple => temple.dedicated))
+
+    }
+
+
+}
+
+
+getTemples();
+
+/* Event Listener */
--- scripts/w05-task.js
+++ scripts/w05-task.js
@@ -1,15 +1,36 @@
 /* W05: Programming Tasks */
 
 /* Declare and initialize global variables */
-const templesElement = getElementId("temples");
+const templesElement = getElementId("div");
+
+var templeList = [];
 
 /* async displayTemples Function */
+const displayTemples = (temples) =>{
+    temples.forEach(temple => {
+        let article = document.createElement("article");
 
+        let templeName = document.createElement("h3");
+        templeName.textContent = temple.templeName;
 
+        let img = document.createElement("img");
+        img.setAttribute("src", temple.imageUrl);
+        img.setAttribute("alt", temple.location);
 
+        article.appendChild(img);
+        article.appendChild(templeName);
+        templesElement.appendChild(article);
+    });
+}
 
 /* async getTemples Function using fetch()*/
-
+const getTemples = async () => {
+    const response = await fetch("https://byui-cse.github.io/cse121b-ww-course/resources/temples.json");
+    
+    templeList = await response.json();
+    output(templeList);
+  };
+  getTemples();
 
 /* reset Function */
 
